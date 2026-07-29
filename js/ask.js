1e783957ae7c1f7a35f7ae79fbd65818d628685a
thandofrans15-dev <thandofrans15@gmail.com>
2026-07-30 01:05:50 +0200

    Create ask.js

---
+++ js/ask.js
@@ -0,0 +1,18 @@
+ask.js
+// Not built yet - just incase i make it to nationals
+
+function mount(container) {
+  container.innerHTML =
+    '<div class="rs-panel" style="position:absolute; top:60px; left:60px; right:60px; bottom:60px; ' +
+    'padding:20px; display:flex; align-items:center; justify-content:center; flex-direction:column;">' +
+    '<h2 style="color:dodgerblue;">Ask</h2>' +
+    '<p style="color:#aaaaaa; max-width:400px; text-align:center;">This part is not finished yet.</p>' +
+    '</div>';
+
+  function unmount() {
+    container.innerHTML = '';
+  }
+  return unmount;
+}
+
+export { mount };
