93a88dc96cad87fde52424b7e96520d6e7ca85c0
thandofrans15-dev <thandofrans15@gmail.com>
2026-07-30 01:10:57 +0200

    Create nuclear-world.js

---
+++ js/nuclear-world.js
@@ -0,0 +1,18 @@
+// nuclear-world.js
+// Not built yet - just a placeholder page for now.
+
+function mount(container) {
+  container.innerHTML =
+    '<div class="rs-panel" style="position:absolute; top:60px; left:60px; right:60px; bottom:60px; ' +
+    'padding:20px; display:flex; align-items:center; justify-content:center; flex-direction:column;">' +
+    '<h2 style="color:dodgerblue;">Nuclear World</h2>' +
+    '<p style="color:#aaaaaa; max-width:400px; text-align:center;">This part is not finished yet.</p>' +
+    '</div>';
+
+  function unmount() {
+    container.innerHTML = '';
+  }
+  return unmount;
+}
+
+export { mount };
