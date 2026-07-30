5fa17024f1829dcba18e629e5848f8ef8f1ee14e
thandofrans15-dev <thandofrans15@gmail.com>
2026-07-30 02:06:04 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,10 +5,10 @@
 */
 import { playIntro } from './intro-globe.js';
 import { mountHub } from './hub-menu.js';
-import * as reactorStudy from './modes/reactor-study.js';
-import * as nuclearFission from './modes/nuclear-fission.js';
-import * as nuclearWorld from './modes/nuclear-world.js';
-import * as ask from './modes/ask.js';
+import * as reactorStudy from './reactor-study.js';
+import * as nuclearFission from './nuclear-fission.js';
+import * as nuclearWorld from './nuclear-world.js';
+import * as ask from './ask.js';
 
 const MODE_MAP = {
   'reactor-study': reactorStudy,
